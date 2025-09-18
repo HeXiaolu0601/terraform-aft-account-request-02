c01e5cabcdad9298b606097a615d98d0aab8f56d
Hayashi Haurki <hayashi.haruki001@jp.panasonic.com>
2025-09-18 10:16:15 +0900

    aws-learn-account003

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,3 +25,31 @@ module "sandbox" {
 
   account_customizations_name = "sandbox"
 }
+
+module "sandbox" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "aws-learning-003@ml.jp.panasonic.com"
+    AccountName               = "aws-learning-003"
+    ManagedOrganizationalUnit = "Sandbox"
+    SSOUserEmail              = "aws-selflearning-env@ml.jp.panasonic.com"
+    SSOUserFirstName          = "AWS Control Tower"
+    SSOUserLastName           = "Admin"
+  }
+
+  account_tags = {
+    "Learn Tutorial" = "AFT"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "HashiCorp Learn"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "sandbox"
+}
